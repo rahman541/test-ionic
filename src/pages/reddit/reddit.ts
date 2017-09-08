27b69da9860fe74699d4d2cb8de0476ca22c90e5
Rahman <rahmanromli@yahoo.com>
2017-09-08 12:13:32 +0800

    View specific item/post in reddit page

--- src/pages/reddit/reddit.ts
+++ src/pages/reddit/reddit.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { RedditService } from '../../services/reddit.service';
 
+import { DetailsPage } from '../details/details';
+
 @Component({
   selector: 'page-reddit',
   templateUrl: 'reddit.html'
@@ -20,6 +22,12 @@ export class RedditPage {
 		this.redditService.getPosts(category, limit).subscribe(response => {
 			this.items = response.data.children;
 			console.log(this.items);
+		});
+	}
+
+	viewItem(item) {
+		this.navCtrl.push(DetailsPage, {
+			item:item
 		});
 	}
 
